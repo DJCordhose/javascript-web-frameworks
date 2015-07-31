c77a9782369379212f5071e755aaa6c77b00ee35
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2015-07-31 17:22:56 +0200

    migrated lehmanns talk to reveal 3.1

---
+++ code/react/build/HelloMessage.js
@@ -0,0 +1,29 @@
+/* @flow */
+
+var ____Class0=React.Component;for(var ____Class0____Key in ____Class0){if(____Class0.hasOwnProperty(____Class0____Key)){HelloMessage[____Class0____Key]=____Class0[____Class0____Key];}}var ____SuperProtoOf____Class0=____Class0===null?null:____Class0.prototype;HelloMessage.prototype=Object.create(____SuperProtoOf____Class0);HelloMessage.prototype.constructor=HelloMessage;HelloMessage.__superConstructor__=____Class0;
+                  
+    function HelloMessage(props) {"use strict";
+        ____Class0.call(this,props);
+        this.state = {greeting: this.props.greeting};
+    }
+    Object.defineProperty(HelloMessage.prototype,"reset",{writable:true,configurable:true,value:function() {"use strict";
+        this.setState({greeting: ""});
+        this.refs.in.getDOMNode().focus();
+    }});
+    Object.defineProperty(HelloMessage.prototype,"updateModel",{writable:true,configurable:true,value:function(event) {"use strict";
+        this.setState({greeting: event.target.value});
+    }});
+    Object.defineProperty(HelloMessage.prototype,"render",{writable:true,configurable:true,value:function() {"use strict";
+        return (
+        React.createElement("div", null, 
+            React.createElement("input", {ref: "in", 
+                   onChange: this.updateModel.bind(this), 
+                   value: this.state.greeting}), 
+            React.createElement("p", null, this.state.greeting, ", World"), 
+            React.createElement("button", {
+                   onClick: this.reset.bind(this)}, "Clear")
+        ));
+    }});
+
+var mountNode = document.getElementById('example');
+React.render(React.createElement(HelloMessage, {greeting: "Hello"}), mountNode);
